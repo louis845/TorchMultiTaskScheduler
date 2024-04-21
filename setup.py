29d294dc13c472f128b611541439d41bb154a284
louis845 <66996988+louis845@users.noreply.github.com>
2024-04-21 16:32:47 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="torchtaskscheduler",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=2.2.2"
+    ],
+    author="Louis Chau",
+    author_email="louis321yh@gmail.com",
+    description="A simple task scheduler for PyTorch tasks.",
+    license="MIT",
+    url="https://github.com/louis845/TorchMultiTaskScheduler"
+)
