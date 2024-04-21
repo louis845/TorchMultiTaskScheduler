5a0880f6eaf3baf3ef57e5cd24a20fdbe6645d2b
louis845 <66996988+louis845@users.noreply.github.com>
2024-04-21 16:35:26 +0800

    Added import for convenience

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchtaskscheduler",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "torch>=2.2.2"
--- torchtaskscheduler/__init__.py
+++ torchtaskscheduler/__init__.py
@@ -1,3 +1,5 @@
 import multiprocessing as mp
 
 assert mp.get_start_method() == "spawn", "Please use spawn start method"
+
+from .scheduler import Scheduler
